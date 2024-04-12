1da7b0e0e1f7c5f6dfd0caf03f92bbbce39b2404
Barogg <juikoff.dima@yandex.ru>
2024-04-13 00:19:48 +0500

    Code update. Moving to another class.

---
+++ cleantalk.classes/Cleantalk.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net.Sockets;
+using System.Net;
+using APIMethods.Pages;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace cleantalk.classes
+{
+	// API
+	public class ApiException : Exception
+	{
+		public ApiException(string message) : base(message) { }
+	}
+
+	// Response
+	public class ApiResponse
+	{
+		public int Spam { get; set; }
+		public int allow { get; set; }
+		public string Comment { get; set; }
+	}
+
+	public static class ClientIpAddressHelper
+	{
+		// Get the client IP address
+		public static string GetClientIpAddress(HttpContext httpContext)
+		{
+			if (httpContext.Request.Headers != null && httpContext.Request.Headers.ContainsKey("X-Forwarded-For"))
+			{
+				string ipAddressList = httpContext.Request.Headers["X-Forwarded-For"];
+				if (!string.IsNullOrEmpty(ipAddressList))
+				{
+					string[] ipAddresses = ipAddressList.Split(",");
+					if (ipAddresses.Length > 0)
+					{
+						string clientIpAddress = ipAddresses[0].Trim();
+						if (IPAddress.TryParse(clientIpAddress, out IPAddress ip))
+						{
+							if (ip.AddressFamily == AddressFamily.InterNetwork)
+							{
+								return clientIpAddress;
+							}
+						}
+					}
+				}
+			}
+
+			// Fallback to the remote IP address
+			string remoteIpAddress = httpContext.Connection.RemoteIpAddress?.ToString() ?? "127.0.0.1";
+			IPAddress ipAddress;
+			if (IPAddress.TryParse(remoteIpAddress, out ipAddress))
+			{
+				if (ipAddress.AddressFamily == AddressFamily.InterNetworkV6)
+				{
+					// Convert IPv6 localhost to IPv4 localhost
+					if (IPAddress.IsLoopback(ipAddress))
+					{
+						return "127.0.0.1";
+					}
+				}
+			}
+			return remoteIpAddress;
+		}
+	}
+
+	// Create an instance of the ApiService
+	public class ApiService
+	{
+		public static HttpClient _httpClient;
+		public static string ApiUrl => "https://moderate.cleantalk.org/api2.0";
+
+		public ApiService(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		// Create an instance of the ApiService
+		public async Task<ApiResponse> CheckMessageAsync(ApiRequest request)
+		{
+			var jsonRequest = JsonConvert.SerializeObject(request);
+			var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+			//var response = await _httpClient.PostAsync(ApiUrl, content);
+			var response = await ApiService._httpClient.PostAsync(ApiService.ApiUrl, content);
+
+			response.EnsureSuccessStatusCode();
+
+			var jsonResponse = await response.Content.ReadAsStringAsync();
+
+			// Print the API response to the console
+			Console.WriteLine("API Response:");
+			Console.WriteLine(jsonResponse);
+			try
+			{
+				var apiResponse = JsonConvert.DeserializeObject<ApiResponse>(jsonResponse);
+				return apiResponse;
+			}
+			catch (JsonReaderException)
+			{
+				throw new ApiException($"Failed to deserialize API response: {jsonResponse}");
+			}
+		}
+	}
+
+}
--- simple.site/Pages/Contact.cshtml.cs
+++ simple.site/Pages/Contact.cshtml.cs
@@ -1,9 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
-using System.Text;
-using System.Net.Sockets;
-using System.Net;
+using cleantalk.classes;
 
 namespace APIMethods.Pages
 {
@@ -37,105 +35,14 @@ namespace APIMethods.Pages
 			_httpContextAccessor = httpContextAccessor;
 		}
 
-		// API
-		public class ApiException : Exception
-		{
-			public ApiException(string message) : base(message) { }
-		}
-
-		// Response
-        public class ApiResponse
-        {
-            public int Spam { get; set; }
-			public int allow { get; set; }
-			public string Comment { get; set; }
-        }
-
-		// Create an instance of the ApiService
-		public class ApiService
-        {
-            private readonly HttpClient _httpClient;
-            private const string ApiUrl = "https://moderate.cleantalk.org/api2.0";
-
-            public ApiService(HttpClient httpClient)
-            {
-                _httpClient = httpClient;
-            }
-
-            public async Task<ApiResponse> CheckMessageAsync(ApiRequest request)
-            {
-				//using var httpClient = new HttpClient();
-				var jsonRequest = JsonConvert.SerializeObject(request);
-                var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
-
-                var response = await _httpClient.PostAsync(ApiUrl, content);
-                response.EnsureSuccessStatusCode();
-
-				 var jsonResponse = await response.Content.ReadAsStringAsync();
-
-				 // Print the API response to the console
-				Console.WriteLine("API Response:");
-				Console.WriteLine(jsonResponse);
-				try
-				{
-					var apiResponse = JsonConvert.DeserializeObject<ApiResponse>(jsonResponse);
-					return apiResponse;
-				}
-				catch (JsonReaderException)
-				{
-					throw new ApiException($"Failed to deserialize API response: {jsonResponse}");
-				}
-			}
-        }
-
 		// GET
         public void OnGet()
         {
         }
 
-		// Get the client IP address
-		private string GetClientIpAddress(HttpContext httpContext)
-		{
-			if (httpContext.Request.Headers != null && httpContext.Request.Headers.ContainsKey("X-Forwarded-For"))
-			{
-				string ipAddressList = httpContext.Request.Headers["X-Forwarded-For"];
-				if (!string.IsNullOrEmpty(ipAddressList))
-				{
-					string[] ipAddresses = ipAddressList.Split(",");
-					if (ipAddresses.Length > 0)
-					{
-						string clientIpAddress = ipAddresses[0].Trim();
-						if (IPAddress.TryParse(clientIpAddress, out IPAddress ip))
-						{
-							if (ip.AddressFamily == AddressFamily.InterNetwork)
-							{
-								return clientIpAddress;
-							}
-						}
-					}
-				}
-			}
-
-			// Fallback to the remote IP address
-			string remoteIpAddress = httpContext.Connection.RemoteIpAddress?.ToString() ?? "127.0.0.1";
-			IPAddress ipAddress;
-			if (IPAddress.TryParse(remoteIpAddress, out ipAddress))
-			{
-				if (ipAddress.AddressFamily == AddressFamily.InterNetworkV6)
-				{
-					// Convert IPv6 localhost to IPv4 localhost
-					if (IPAddress.IsLoopback(ipAddress))
-					{
-						return "127.0.0.1";
-					}
-				}
-			}
-			return remoteIpAddress;
-		}
-
 		// Your auth key: https://cleantalk.org/my
 		public const string AuthKey = "your_auth_key";
-
+		
 		// POST
 		public async Task OnPostAsync()
         {
@@ -158,7 +65,7 @@ namespace APIMethods.Pages
 			int submitTime = int.TryParse(Request.Form["pageLoadTime"], out int result) ? result : 0;
 			// Calculate submit_time and js_on parameters
 
-			var senderIp = GetClientIpAddress(HttpContext);
+			var senderIp = ClientIpAddressHelper.GetClientIpAddress(HttpContext);
 
 			// Get all headers
 			var headersDictionary = HttpContext.Request.Headers.ToDictionary(h => h.Key, h => h.Value);
